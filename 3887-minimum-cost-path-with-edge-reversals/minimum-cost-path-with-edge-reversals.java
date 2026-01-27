5dbb8895a77bdec30aa88139bd7f27d7748feafc
Bodda Gopala Krishna <142887883+22p31a05e6@users.noreply.github.com>
2026-01-27 20:43:09 +0530

    Time: 217 ms (13.36%) | Memory: 238.9 MB (74.09%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -1,55 +1,64 @@
 class Solution {
-
+    class Edge{
+        int wt;
+        Node node;
+        public Edge(int wt, Node node){
+            this.wt = wt;
+            this.node = node;
+        }
+    }
+    class Node{
+        int id;
+        Set<Edge> set;
+        public Node(int id){
+            this.id = id;
+            this.set = new HashSet<>();
+        }
+    }
+    class Trip{
+        int cost;
+        Node n;
+        public Trip(int cost, Node n){
+            this.cost = cost;
+            this.n = n;
+        }
+    }
     public int minCost(int n, int[][] edges) {
-        List<int[]>[] g = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            g[i] = new ArrayList<>();
+        Node arr[] = new Node[n];
+        int cost[] = new int[n];
+        for(int i = 0;i < n;i++){
+            arr[i] = new Node(i);
+            cost[i] = Integer.MAX_VALUE;
         }
-
-        for (int[] e : edges) {
-            int x = e[0];
-            int y = e[1];
-            int w = e[2];
-            g[x].add(new int[] { y, w });
-            g[y].add(new int[] { x, 2 * w });
+        for(int i = 0;i < edges.length;i++){
+            Node a = arr[edges[i][0]];
+            Node b = arr[edges[i][1]];
+            int wt = edges[i][2];
+            a.set.add(new Edge(wt, b));
+            b.set.add(new Edge(wt*2, a));
         }
-
-        // Dijkstra
-        int[] d = new int[n];
-        boolean[] visited = new boolean[n];
-        Arrays.fill(d, Integer.MAX_VALUE);
-        d[0] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            Comparator.comparingInt(a -> a[0])
-        );
-        pq.offer(new int[] { 0, 0 }); // [Distance, Node]
-
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            int dist = current[0];
-            int x = current[1];
-
-            if (x == n - 1) {
-                return dist;
-            }
-
-            if (visited[x]) {
+        cost[0] = 1;
+        Set<Node> set = new HashSet<>();
+        Queue<Trip> queue = new PriorityQueue<Trip>((Trip a, Trip b)->{
+            return Integer.compare(a.cost, b.cost);
+        });
+        queue.add(new Trip(0, arr[0]));
+        while(queue.size() != 0 && !set.contains(arr[n-1])){
+            Trip t = queue.poll();
+            if(t.cost >= cost[t.n.id]){
                 continue;
             }
-            visited[x] = true;
-
-            for (int[] neighbor : g[x]) {
-                int y = neighbor[0];
-                int w = neighbor[1];
-
-                if (dist + w < d[y]) {
-                    d[y] = dist + w;
-                    pq.offer(new int[] { d[y], y });
+            set.add(arr[t.n.id]);
+            cost[t.n.id] = t.cost;
+            // System.out.printf("visited: %d\n", t.n.id);
+            for(Edge e: arr[t.n.id].set){
+                if(t.cost + e.wt < cost[e.node.id]){
+                    queue.add(new Trip(t.cost + e.wt, e.node));
                 }
             }
         }
-
-        return -1;
+        if(cost[n-1] == Integer.MAX_VALUE)
+            return -1;
+        return cost[n-1];
     }
 }
