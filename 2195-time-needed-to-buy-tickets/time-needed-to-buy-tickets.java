4d17d55b8bd03bb65f10fd2d6b7868b642357021
Bodda Gopala Krishna <142887883+22p31a05e6@users.noreply.github.com>
2026-02-02 16:44:18 +0530

    Time: 9 ms (29.23%) | Memory: 46.5 MB (6.24%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+      Queue<Integer> qu = new LinkedList<>();
+      for(int i =0;i<tickets.length;i++){
+        qu.offer(i);
+      } 
+      int time =0;
+      while(true){
+        int p = qu.poll();
+        tickets[p]--;
+        time++;
+        if(p == k && tickets[p]== 0){
+            return time;
+        }
+        if(tickets[p]>0)
+        qu.offer(p);
+      } 
+    }
+}
