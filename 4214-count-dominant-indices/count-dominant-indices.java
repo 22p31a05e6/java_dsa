beb64b0786f4687ccd7221c74178fec2787a4c53
Bodda Gopala Krishna <142887883+22p31a05e6@users.noreply.github.com>
2026-02-08 19:35:05 +0530

    Time: 1 ms (99.97%) | Memory: 46.3 MB (67.59%) - LeetSync

---
+++ 4214-count-dominant-indices/count-dominant-indices.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int dominantIndices(int[] nums) {
+     int n = nums.length;
+        int count = 0;
+
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+
+        for(int i = 0; i < n - 1; i++) {
+
+            sum -= nums[i];   
+
+            int rightCount = n - i - 1;
+
+            if(nums[i] > (double)sum / rightCount) {
+                count++;
+            }
+        }
+
+        return count;   
+    }
+}
